a4cefd83ea217ae4030e8bc0eb46e3aef4a0f242
xzb <1102059895@qq.com>
2020-09-08 15:25:07 +0800

    项目修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -19,7 +19,8 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['token'] = getToken()
+      config.headers["Content-Type"] = "application/json"
+      config.headers['HOME-API-TOKEN'] = getToken()
     }
     return config
   },
@@ -45,7 +46,7 @@ service.interceptors.response.use(
   response => {
     const res = response.data
     // if the custom code is not 20000, it is judged as an error.
-    if (res.errorCode !== '000000') {
+    if (response.status !== '200') {
       Message({
         message: res.message || 'Error',
         type: 'error',
